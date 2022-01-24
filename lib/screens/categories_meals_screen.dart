808e69b67656ff1e35df75bc2bbde24bb6424228
Erivaldo Lourenço <erivaldolourenco.m@gmail.com>
2022-01-24 00:22:21 -0300

    Adiciona dados mokados e cria modelo de meals

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
     final category = ModalRoute.of(context)!.settings.arguments as Category;
 
-   return Scaffold(
-     appBar: AppBar(
-       title: Text(category.title),
-     ),
-     body: Center(child: Text('Receitas por Categoria'),),
-   );
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (ctx, index){
+          return Text(categoryMeals[index].title);
+        },
+      ),
+    );
   }
 }
