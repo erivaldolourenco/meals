b6716eff2daf7ccb0368a46bd247e81a9bcdc767
Erivaldo Lourenço <erivaldolourenco.m@gmail.com>
2022-01-24 00:01:42 -0300

    Adiciona rotas nomeadas

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,46 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:meals/utils/app_routes.dart';
 import '../models/category.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final Category category;
 
   const CategoryItem(this.category);
 
-  void _selectCategory(BuildContext context){
-    Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (_){
-            return CategoriesMealsScreen(category);
-          },
-    ));
+  void _selectCategory(BuildContext context) {
+    // Navigator.of(context).push(
+    //     MaterialPageRoute(
+    //       builder: (_){
+    //         return CategoriesMealsScreen(category);
+    //       },
+    // ));
+    Navigator.of(context)
+        .pushNamed(AppRoutes.CATEGORIES_MEALS, arguments: category);
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return InkWell(
       onTap: () => _selectCategory(context),
       splashColor: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: EdgeInsets.all(15),
-        child: Text(category.title,
+        child: Text(
+          category.title,
           style: Theme.of(context).textTheme.caption,
         ),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-            colors: [
+          gradient: LinearGradient(colors: [
             category.color.withOpacity(0.5),
             category.color,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          ),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
         ),
       ),
     );
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/utils/app_routes.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,19 +13,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        secondaryHeaderColor: Colors.amber,
-        fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          caption: TextStyle(
-            fontSize: 20,
-            fontFamily: 'RobotoCondensed'
-          )
-        )
-
-      ),
-      home: CategoriesScreen(),
+          primarySwatch: Colors.pink,
+          secondaryHeaderColor: Colors.amber,
+          fontFamily: 'Raleway',
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+              caption: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'))),
+      routes: {
+        AppRoutes.HOME: (ctx) => CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
